6ce7c841eec4717cb6be5de8a89185b7bd8a1da1
dimas <garcia.dimas.siete@gmail.com>
2020-03-08 02:08:10 -0600

    repo

--- svi/clinicaveterinaria/src/main/java/com/svi/clinicaveterinaria/model/dao/clienterepository.java
+++ svi/clinicaveterinaria/src/main/java/com/svi/clinicaveterinaria/model/dao/clienterepository.java
@@ -2,8 +2,6 @@ package com.svi.clinicaveterinaria.model.dao;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.svi.clinicaveterinaria.model.entidad.informacion;
-
 public interface clienterepository extends CrudRepository<informacion, String> {
-
+ 
 }
